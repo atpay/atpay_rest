8b3dfd0da75c2426ea2c432563c0ac35e9b52404
Isaiah Baca <isaiah@Isaiahs-MacBook-Pro-2.local>
2016-02-12 00:58:57 -0700

    Adds thanks_url

--- lib/atpay_rest/atpay_link.rb
+++ lib/atpay_rest/atpay_link.rb
@@ -5,12 +5,14 @@ module AtpayRest
   class AtpayLink
     attr_accessor :long_url
     attr_accessor :short_url
+    attr_accessor :thanks_url
 
     def initialize(session, link)
       _response   = session.conn.post("shorten", {url: link})
       response    = JSON.parse _response.body
       @short_url  = response["atpay_link"]
       @long_url   = response["long_url"]
+      @thanks_url = response["thanks_url"]
     end
   end
 end
