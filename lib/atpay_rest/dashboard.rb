83b0e7d293825050083d768811a9bfb2f6d82543
Isaiah Baca <isaiah@atpay.com>
2016-04-02 00:01:06 -0600

    log response

--- lib/atpay_rest/dashboard.rb
+++ lib/atpay_rest/dashboard.rb
@@ -5,6 +5,7 @@ module AtpayRest
   class Dashboard
     def self.find_organization(session, search_string, args={})
       response = session.conn.get "api/v4/rest/organizations/#{search_string}", args
+      puts response.body
       JSON.parse response.body
     end
 
