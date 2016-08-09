9a6891ac68672dd9060c85831350da92ff29f498
Isaiah Baca <isaiah@atpay.com>
2016-08-09 16:28:01 -0600

    Rails 5 does not like autoload_paths... fix later

--- lib/atpay_rest.rb
+++ lib/atpay_rest.rb
@@ -1,7 +1,4 @@
 module AtpayRest
-  if defined?(Rails)
-    require_relative "engine"
-  else
     require_relative "atpay_rest/session"
     require_relative "atpay_rest/version"
     require_relative "atpay_rest/payment_method"
@@ -9,6 +6,4 @@ module AtpayRest
     require_relative "atpay_rest/atpay_link"
     require_relative "atpay_rest/dashboard"
     require_relative "atpay_rest/campaign"
-
-  end
 end
